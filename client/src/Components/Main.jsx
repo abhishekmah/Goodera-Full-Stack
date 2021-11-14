fb90b605ea31f30d3abeed1daba13f2d616f7375
Abhishek Mahato <mahatoabhishek123@gmail.com>
2021-11-14 14:32:23 +0530

    update

--- client/src/Components/Main.jsx
+++ client/src/Components/Main.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from 'react';
+import styles from './Main.module.css';
+import Navbar from './Navbar';
 
 const Main = () => {
     return (
-        <div>
-            
+        <div className={styles.main}>
+            <div className={styles.blue}>
+                <div className={styles.box}>
+                    <div className={styles.nav}>
+                        <div className={styles.left}>
+                            <button>JobHunt</button>
+                            <button>Find Jobs</button>
+                            <button>Upskill Yourself</button>
+                        </div>
+                        <div className={styles.right}></div>
+                    </div>
+                </div>
+            </div>
         </div>
-    )
-}
+    );
+};
 
-export default Main
+export default Main;
--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -3,8 +3,15 @@ import styles from "./Navbar.module.css"
 
 const Navbar = () => {
     return (
-        <div>
-            
+        <div className={styles.nav}>
+            <div className={styles.left}>
+                <button>JobHunt</button>
+                <button>Find Jobs</button>
+                <button>Upskill Yourself</button>
+            </div>
+            <div className={styles.right}>
+
+            </div>
         </div>
     )
 }
